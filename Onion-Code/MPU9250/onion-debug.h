a2217309611ee84e5d05250328d3a8bb2d47cf7c
Oliver Rettig <Oliver.rettig@orat.de>
2018-05-14 15:49:02 +0200

    Update onion-debug.h

--- Onion-Code/MPU9250/onion-debug.h
+++ Onion-Code/MPU9250/onion-debug.h
@@ -5,14 +5,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-#define ONION_SEVERITY_FATAL			(-1)
-#define ONION_SEVERITY_INFO				(0)
-#define ONION_SEVERITY_DEBUG			(1)
+#define ONION_SEVERITY_FATAL			    (-1)
+#define ONION_SEVERITY_INFO				    (0)
+#define ONION_SEVERITY_DEBUG			    (1)
 #define ONION_SEVERITY_DEBUG_EXTRA		(2)
 
-#define ONION_VERBOSITY_NONE			(ONION_SEVERITY_FATAL)
-#define ONION_VERBOSITY_NORMAL			(ONION_SEVERITY_INFO)
-#define ONION_VERBOSITY_VERBOSE			(ONION_SEVERITY_DEBUG)
+#define ONION_VERBOSITY_NONE			    (ONION_SEVERITY_FATAL)
+#define ONION_VERBOSITY_NORMAL			  (ONION_SEVERITY_INFO)
+#define ONION_VERBOSITY_VERBOSE			  (ONION_SEVERITY_DEBUG)
 #define ONION_VERBOSITY_EXTRA_VERBOSE	(ONION_SEVERITY_DEBUG_EXTRA)
 
 
@@ -26,10 +26,10 @@ extern
 int 	onionVerbosityLevel;
 
 // debug functions
-void 	onionSetVerbosity		(int verbosityLevel);
-int 	onionGetVerbosity 		();
+void 	onionSetVerbosity(int verbosityLevel);
+int 	onionGetVerbosity();
 
-void 	onionPrint 				(int severity, const char* msg, ...);
+void 	onionPrint(int severity, const char* msg, ...);
 
 #ifdef __cplusplus
 }
